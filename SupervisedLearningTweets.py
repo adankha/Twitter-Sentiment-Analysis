d6e41c25ca4d3cf6b859e941b50ff91edbe3b885
Ashour Dankha <dankha90@gmail.com>
2017-11-03 12:02:39 -0500

    normalized data

--- SupervisedLearningTweets.py
+++ SupervisedLearningTweets.py
@@ -5,6 +5,7 @@ import nltk
 import json
 import time
 import inflect
+from itertools import groupby
 p = inflect.engine()
 
 # TODO: Find a better way to figure out if a Tweet is in English or not [CURRENT: Using goslate, but too many calls to
@@ -51,15 +52,17 @@ NAIVE APPROACH:
     # Increase time to 3 seconds, but reduce words down to 2.6k
 """
 
+
 def add_to_dict(stop_words, row, word, words):
 
     if word not in stop_words:
+
         singular_word = p.singular_noun(word)
+
         if singular_word is not False:
             word = singular_word
 
         if word not in words:
-
             if row['classification'] == '-1':
                 words[word] = [1, 0, 0, 0]
             elif row['classification'] == '0':
@@ -69,7 +72,6 @@ def add_to_dict(stop_words, row, word, words):
             elif row['classification'] == '2':
                 words[word] = [0, 0, 0, 1]
         else:
-
             if row['classification'] == '-1':
                 words[word][0] += 1
             elif row['classification'] == '0':
@@ -82,13 +84,19 @@ def add_to_dict(stop_words, row, word, words):
 
 def read_lines(reader, words, stop_words):
 
-    regex = re.compile(r'<.*?>|https?[^ ]+|([@])[^ ]+|[^a-zA-Z ]+|\d+/?')
-
+    # Current Regex Removes:
+    # Links
+    # Tags to other people (@Obama)
+    # Hashtag words
+    # Anything not an alphabet
+    # All digits
+    regex = re.compile(r'<.*?>|https?[^ ]+|([@#])[^ ]+|[^a-zA-Z ]+|\d+/?')
 
     for row in reader:
         line = row['Anootated tweet']
         line = regex.sub(' ', line)
         line = re.sub(' +', ' ', line).strip()
+        line = ''.join(''.join(s)[:2] for _, s in groupby(line))
         line = line.encode('ascii', errors='ignore').decode('utf-8').lower()
         line = line.split(' ')
         for word in line:
@@ -103,7 +111,8 @@ def main():
     # Here I create a set for "constant" look-up.
     # Reduced time by 37 seconds.
     stop_words = set()
-    words = {}
+    obama_freq_words = {}
+    romney_freq_words = {}
     list_stop_words = nltk.corpus.stopwords.words('english')
     for word in list_stop_words:
         stop_words.add(word)
@@ -112,17 +121,25 @@ def main():
 
         reader = csv.DictReader(csvfile)
         reader2 = csv.DictReader(csvfile2)
-        read_lines(reader, words, stop_words)
-        read_lines(reader2, words, stop_words)
+        read_lines(reader, obama_freq_words, stop_words)
+        read_lines(reader2, romney_freq_words, stop_words)
 
-    for key in words:
-        print('key: ', key, ' ', words[key])
+    for key in obama_freq_words:
+        total = sum(obama_freq_words[key])
+        obama_freq_words[key] = [(float(i)/total) for i in obama_freq_words[key]]
+        print('obama_key: ', key, ' ', obama_freq_words[key],
+              'index of max: ', obama_freq_words[key].index(max(obama_freq_words[key])))
 
-    # json_words = json.dumps(words)
-    # loaded_words = json.loads(json_words)
-    # print(loaded_words)
+    for key in romney_freq_words:
+        total = sum(romney_freq_words[key])
+        romney_freq_words[key] = [(float(i)/total) for i in romney_freq_words[key]]
+        print('romney_key: ', key, ' ', romney_freq_words[key],
+              'index of max: ', romney_freq_words[key].index(max(romney_freq_words[key])))
 
-    print('Total number of words: ', len(words))
+
+    print('obama words: ', len(obama_freq_words))
+    print('romney words: ', len(romney_freq_words))
+
     end = time.time()
     print('Total time: ', (end - start) * 1000)
 
